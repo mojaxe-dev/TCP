e574f2d1fd0c84335e5ca2a6604cd47948993f83
Harris <harriswaltinjoy@gmail.com>
2026-02-19 10:25:18 +0530

    creating CTA

---
+++ src/CTA.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function CTA() {
+
+
+    return (
+        <div className="bg-[#9c3466] ">
+            <h1>Enjoy Unlimited</h1>
+        </div>
+    )
+}
