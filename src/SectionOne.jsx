ed34acb984030b1b7a04ab1311913aaa5fbab4ba
Roger Waltin <Roger.mwaltin@gmail.com>
2026-02-19 16:20:26 +0530

    finished section-1

--- src/SectionOne.jsx
+++ src/SectionOne.jsx
@@ -6,7 +6,7 @@ function SectionOne() {
     const underlineEffect = "after:content-[''] after:w-0 after:h-0.5 after:bg-black after:absolute relative after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-500 w-max hover:cursor-pointer"
 
     return (
-        <div className="flex ml-30 mr-30 mt-10 gap-6 mb-10">
+        <div className="flex mx-32 my-10 gap-5">
             <div id="left-side" className="flex flex-col border-r border-gray-300 pr-6">
                 <img src={whatGospel} alt="what_gospel" className="w-2xs hover:cursor-pointer" />
                 <a href="" className={`${underlineEffect} mt-3 text-2xl font-serif`}>What is the Gospel?</a>
@@ -18,16 +18,71 @@ function SectionOne() {
                 <span className="mt-3 hover:text-gray-500 hover:cursor-pointer transition duration-300 w-max">Ashok Mohanakumar</span>
             </div>
 
-            <div id="center" className="">
-                <img src={unshakable} alt="what_gospel" className="w-156 mb-4" />
-                <div className="flex justify-center">
-                    <span className="font-semibold text-[0.7rem] tracking-widest">FEATURED POST</span>
-                    <span></span>
+            <div id="center" className="pr-6 border-r border-gray-300 flex flex-col items-center max-w-160 w-full ">
+                <img src={unshakable} alt="unshakable" className=" mb-4" />
+                <div className="flex justify-center gap-6">
+                    <span className="font-semibold text-[0.8rem] tracking-widest group text-[#9c3466] transition duration-500 hover:text-gray-500 hover:cursor-pointer"><span className="inline-block w-2.5 h-2.5 rounded-full bg-[#9c3466] mr-2 group-hover:bg-gray-500 transition duration-300"></span>FEATURED POST</span>
+                    <span className="text-[0.8rem] text-gray-500">By <span className="text-black font-semibold hover:text-gray-500 transition duration-300 hover:cursor-pointer">Ashok Mohanakumar</span></span>
+
                 </div>
+                <span className={`${underlineEffect} text-lg font-serif mt-2`}>The Unshakable Ones of Zion</span>
+                <p className="text-md font-serif mt-2">Gold appears frequently in the Bible, from the rivers of Eden to the gifts of the Magi. The use of gold is often symbolic of something of high value or honor. The Bible recounts that gold was a gift given to Christ at his birth. This act emphasizes that gold is a fitting tribute for …</p>
             </div>
 
-            <div id="right-side">
+            <div id="right-side" className="flex flex-col">
+                <div className="flex gap-2">
+                    <div className="flex flex-col">
+                        <a href="" className={`${underlineEffect} text-lg font-serif min-w-0 max-w-50 font-semibold`}>The Unshakable Ones of Zion</a>
+                        <span className="text-[0.8rem] text-gray-500">By <span className="text-black font-semibold hover:text-gray-500 transition duration-300 hover:cursor-pointer">Ashok Mohanakumar</span></span>
+                    </div>
+                    <div className="w-24 h-24 overflow-hidden">
+                        <img src={unshakable} alt="unshakable" className="w-[80%] h-[80%] object-cover"/>
+                    </div>
+                </div>
+                <hr className="mt-4 text-gray-200 mb-4" />
 
+                <div className="flex gap-2">
+                    <div className="flex flex-col">
+                        <a href="" className={`${underlineEffect} text-lg font-serif min-w-0 max-w-50 font-semibold`}>A Biblical Theology of Gold and Glory</a>
+                        <span className="text-[0.8rem] text-gray-500">By <span className="text-black font-semibold hover:text-gray-500 transition duration-300 hover:cursor-pointer">Ashok Mohanakumar</span></span>
+                    </div>
+                    <div className="w-24 h-24 overflow-hidden">
+                        <img src="https://thecrosspurpose.com/wp-content/uploads/2025/08/gold_banner-100x100.png" alt="gold" className="w-[80%] h-[80%] object-cover"/>
+                    </div>
+                </div>
+                <hr className="mt-4 text-gray-200 mb-4" />
+
+                <div className="flex gap-2">
+                    <div className="flex flex-col">
+                        <a href="" className={`${underlineEffect} text-lg font-serif min-w-0 max-w-50 font-semibold`}>Fatherhood And The Imitation of God</a>
+                        <span className="text-[0.8rem] text-gray-500">By <span className="text-black font-semibold hover:text-gray-500 transition duration-300 hover:cursor-pointer">Ashok Mohanakumar</span></span>
+                    </div>
+                    <div className="w-24 h-24 overflow-hidden">
+                        <img src="https://thecrosspurpose.com/wp-content/uploads/2025/08/ChatGPT-Image-Aug-5-2025-10_22_10-PM-100x100.png" alt="fatherhood" className="w-[80%] h-[80%] object-cover"/>
+                    </div>
+                </div>
+                <hr className="mt-4 text-gray-200 mb-4" />
+
+                <div className="flex gap-2">
+                    <div className="flex flex-col">
+                        <a href="" className={`${underlineEffect} text-lg font-serif min-w-0 max-w-50 font-semibold`}>No Slavery to Christ, No Heaven!</a>
+                        <span className="text-[0.8rem] text-gray-500">By <span className="text-black font-semibold hover:text-gray-500 transition duration-300 hover:cursor-pointer">Pankaj</span></span>
+                    </div>
+                    <div className="w-24 h-24 overflow-hidden">
+                        <img src="https://thecrosspurpose.com/wp-content/uploads/2025/07/strong_linked_chain-100x100.webp" alt="slavery" className="w-[80%] h-[80%] object-cover"/>
+                    </div>
+                </div>
+                <hr className="mt-4 text-gray-200 mb-4" />
+
+                <div className="flex gap-2">
+                    <div className="flex flex-col">
+                        <a href="" className={`${underlineEffect} text-lg font-serif min-w-0 max-w-50 font-semibold`}>Obedience is better than Sacrifice</a>
+                        <span className="text-[0.8rem] text-gray-500">By <span className="text-black font-semibold hover:text-gray-500 transition duration-300 hover:cursor-pointer">Ashok Mohanakumar</span></span>
+                    </div>
+                    <div className="w-24 h-24 overflow-hidden">
+                        <img src="https://thecrosspurpose.com/wp-content/uploads/2024/12/lamb-5733685_1280.jpg" alt="obedience" className="w-[80%] h-[80%] object-cover"/>
+                    </div>
+                </div>
             </div>
 
         </div>
