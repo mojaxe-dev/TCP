b713c3c6d8ef3a883bf10a69ae77e21afb088d93
Harris <harriswaltinjoy@gmail.com>
2026-02-20 13:05:21 +0530

    added one half

--- src/Section5.jsx
+++ src/Section5.jsx
@@ -1,9 +1,13 @@
-import { FaArrowRight } from "react-icons/fa";
+import { FaArrowAltCircleRight, FaArrowCircleRight, FaArrowRight, FaArrowsAlt, FaRegArrowAltCircleRight } from "react-icons/fa";
 import meetmario from "./assets/meetmario.jpg"
-
+import "./styles.css"
 
 export default function Section5() {
     const underlineEffect = "after:content-[''] after:w-0 after:h-0.5 after:bg-black after:absolute relative after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-500 w-max hover:cursor-pointer"
+
+    function handleUnderline() {
+
+    }
 
     return (
         <div className="mx-32">
@@ -18,10 +22,13 @@ export default function Section5() {
                 <div className="flex border-r pr-5 border-gray-300 whitespace-nowrap">
                     <img className="w-70" src={meetmario} alt="mario" />
                     <ul className="flex-col ml-8 mt-18">
-                        <li><p><span className="text-[#9c3466] hover:text-gray-500">● Featured Post</span> <span className="hover:text-[#9c3466]">Podcast</span> <span className="hover:text-[#9c3466]">The Show</span></p></li>
-                        <li className="mt-2"><p className="leading-none m-0"><span className="block font-bold text-xl">Meet Pastor Mario - </span><span className="block font-bold text-xl -mt-2">Reformation in India</span></p></li>
+                        <li><p className="text-xs font-semibold tracking-wider whitespace-break-spaces"><a href="#"><span className="text-[#9c3466] hover:text-gray-500"><span className="text-lg">●</span> FEATURED POST</span></a>     <a href="#"><span className="hover:text-[#9c3466]">PODCAST</span></a>     <a href="#"><span className="hover:text-[#9c3466]">THE SHOW</span></a></p></li>
+                        <ul className="underlineGroup mt-2">
+                            <li className="underlineEffect font-bold text-xl">Meet Pastor Mario -</li>
+                            <li className="underlineEffect font-bold text-xl -mt-1.5">Reformation in India</li>
+                        </ul>
                         <li className="mt-4"><p className="text-sm"><span className="block">🎙️ Podcast Episode: Meet Pastor Mario - </span><span className="block">Reformation in India 🇮🇳 Join us for an awesome</span><span className="block"> episode as we sit …</span></p></li>
-                        <li className="mt-4">Play Episode</li>
+                        <li className="mt-4 font-bold flex flex-row gap-3 hover:text-[#9c3466]">Play Episode <FaRegArrowAltCircleRight size={20} className="mt-1" /></li>
                     </ul>
                 </div>
                 <div>
